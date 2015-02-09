d72d8c0006132600187f12f162df6e7098ce8655
chengshq <horizonsky123@gmail.com>
2015-02-08 22:40:39 -0600

    hw5

---
+++ machine.py
@@ -0,0 +1,14 @@
+class CoinDispenser:
+
+	coins = [25, 10, 5, 1]
+
+	def __init__(self):
+		pass
+
+	def make_change(self, amount):
+		coin_numbers = []
+		for coin in self.coins:
+			coin_number = amount // coin
+			amount = amount % coin
+			coin_numbers.append(coin_number)
+		return coin_numbers
